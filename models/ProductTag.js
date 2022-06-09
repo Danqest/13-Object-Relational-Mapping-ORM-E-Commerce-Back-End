6b5fc8ab2004e5d044cad4d8ee29d9b7de609977
Colin Brooks <mcbrooks@pm.me>
2022-06-09 12:18:53 -0400

    reverted model column names

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -12,12 +12,12 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true
     },
-    productId: {
+    product_id: {
       type: DataTypes.INTEGER,
       references: 'Product',
       referencesKey: 'id'
     },
-    tagId: {
+    tag_id: {
       type: DataTypes.INTEGER,
       references: 'Tag',
       referencesKey: 'id'
