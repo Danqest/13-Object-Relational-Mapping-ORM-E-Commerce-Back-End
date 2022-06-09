7da1d89f481aaf901074fc4d181aa38ce8afee42
Colin Brooks <mcbrooks@pm.me>
2022-06-09 07:15:07 -0400

    updated file folder structure

---
+++ models/ProductTag.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      references: 'Product',
+      referencesKey: 'id'
+    },
+    tagId: {
+      type: DataTypes.INTEGER,
+      references: 'Tag',
+      referencesKey: 'id'
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
